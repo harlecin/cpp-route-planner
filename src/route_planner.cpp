8e65f3e32398a62a469614948ab1664af436b0f1
Christoph <christoph.bodner@gmail.com>
2021-02-07 16:06:48 +0000

    find closest nodes for start/end and add to attr

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -2,6 +2,8 @@
 #include <algorithm>
 
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
+    // m_Model(model) is initialized
+
     // Convert inputs to percentage:
     start_x *= 0.01;
     start_y *= 0.01;
@@ -10,7 +12,11 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 
     // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
+    auto start_node = m_Model.FindClosestNode(start_x, start_y);
+    auto end_node = m_Model.FindClosestNode(end_x, end_y);
 
+    this->start_node = &start_node;
+    this->end_node = &end_node;
 }
 
 
