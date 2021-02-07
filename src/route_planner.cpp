55205213fc751e0c2067534cc6535b8d8a884301
Christoph <christoph.bodner@gmail.com>
2021-02-07 16:25:12 +0000

    fixed findclosest and add CalculateHValue()

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
-    // m_Model(model) is initialized
+    // m_Model(model) is initialized using an initializer list
 
     // Convert inputs to percentage:
     start_x *= 0.01;
@@ -12,11 +12,9 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 
     // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
-    auto start_node = m_Model.FindClosestNode(start_x, start_y);
-    auto end_node = m_Model.FindClosestNode(end_x, end_y);
+    start_node = &m_Model.FindClosestNode(start_x, start_y);
+    end_node = &m_Model.FindClosestNode(end_x, end_y);
 
-    this->start_node = &start_node;
-    this->end_node = &end_node;
 }
 
 
@@ -24,11 +22,10 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 // Tips:
 // - You can use the distance to the end_node for the h value.
 // - Node objects have a distance method to determine the distance to another node.
-
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
-
+    RouteModel::Node current_node = *node;
+    return current_node.distance(*end_node);
 }
-
 
 // TODO 4: Complete the AddNeighbors method to expand the current node by adding all unvisited neighbors to the open list.
 // Tips:
